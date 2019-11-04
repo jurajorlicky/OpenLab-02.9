ebdeb9b7dbb86252fcab8dcad203629059b61e8d
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:36:01 +0100

    Add project files.

---
+++ OpenLab 02.9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OpenLab_02._9
+{
+    class Program
+    {
+        public static string menopriezvisko(string firstname, string lastname)
+        {
+            return (lastname + " , " + firstname);
+        }
+        static void Main(string[] args)
+        {
+            string firstname = "Juraj";
+            string lastname = "Orlický";
+            Console.WriteLine(menopriezvisko(firstname, lastname));
+        }
+    }
+}
